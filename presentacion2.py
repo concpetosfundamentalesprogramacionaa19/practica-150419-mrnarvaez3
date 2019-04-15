4e207b428c6cbfe8a972c512b6636e94ac9d63e9
mrnarvaez3 <robertoc_98_2009@hotmail.com>
2019-04-15 16:44:43 -0500

    ejemplo

---
+++ presentacion2.py
@@ -0,0 +1,6 @@
+"""
+	@mrnarvaez3
+"""
+nombre = input("Por favor ingrese su nombre:\n")
+apellido = input ("Por favor ingrese su apellido:\n")
+print("Mi nombre es: %s\n \t Mi apellido es: \n %s" % (nombre, apellido))
